7baac2771dc3d7637cc61487e15c32a87193e7b5
Duc Do <dotrananhduc@gmail.com>
2017-05-23 21:33:35 +0700

    Update

---
+++ main.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <fstream>
+
+using namespace std;
+
+const int TABLE_SIZE = 128;
+
+class HashNode {
+private:
+    string value;
+    string key;
+    HashNode *pNext;
+public:
+    HashNode(string key, string value) {
+        this->key = key;
+        this->value = value;
+        this->pNext = NULL;
+    }
+
+    string getKey() {
+        return key;
+    }
+
+    string getValue() {
+        return value;
+    }
+
+    HashNode* getNext(){
+        return pNext;
+    }
+
+    void setNext(HashNode* p) {
+        this->pNext = p;
+    }
+};
+
+class Hashmap {
+private:
+    HashNode **table;
+public:
+    Hashmap() {
+        table = new HashNode*[TABLE_SIZE];
+        for(int i = 0; i < 128; i++) {
+            table[i] = NULL;
+        }
+    }
+
+    ~Hashmap() {
+        for(int i = 0; i < TABLE_SIZE; i++) {
+            HashNode* entry = table[i];
+            while(entry != NULL) {
+                HashNode* p = entry;
+                entry = entry->getNext();
+                delete p;
+            }
+        }
+        delete[] table;
+    }
+
+    int createHash(string key) {
+        long sum = 0;
+        long len = key.length();
+        for(int i = 0; i < len; i++){
+            sum += (int)key[i] * pow(128 * 1.0, len * 1.0);
+            sum += sum % TABLE_SIZE;
+            len--;
+        }
+        return sum;
+    }
+
+    void Insert(string key, string value) {
+        int hash = createHash(key);
+        if(table[hash] == NULL) {
+            table[hash] = new HashNode(key, value);
+        }
+        else {
+            HashNode* entry = table[hash];
+            while(entry->getNext() != NULL) {
+                entry = entry->getNext();
+            }
+            entry->setNext(new HashNode(key, value));
+        }
+    }
+
+    string Search(string key) {
+        int hash = createHash(key);
+        if(table[hash] == NULL) {
+            return "Not found";
+        }
+        else {
+            HashNode* entry = table[hash];
+            while(entry != NULL && entry->getKey() != key) {
+                entry = entry->getNext();
+            }
+            if(entry == NULL) {
+                return "Not found";
+            }
+            return entry->getValue();
+        }
+    }
+};
+
+int main()
+{
+    ifstream file("dic.dat", ios_base::in);
+    string str;
+    getline(file, str);
+
+    string key = "", val = "";
+    int vt = -1;
+    for(unsigned int i = 0; i < str.length(); i++) {
+        if(str[i] == ':'){
+            vt = i + 1;
+            break;
+        }
+        key += str[i];
+    }
+
+    for(unsigned int i = vt; i < str.length(); i++){
+        val += str[i];
+    }
+
+    cout << key << endl << val;
+
+    return 0;
+}
